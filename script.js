147c7f69caaeebac6a9cb8e6814d73ac3a4f8f63
ricarvsan <ricarvsan@gmail.com>
2023-04-15 15:34:27 -0300

    semi completo sem bonus para teste hub

--- script.js
+++ script.js
@@ -1,4 +1,116 @@
-function listaParticipantes() {
+axios.defaults.headers.common['Authorization'] = 'zTIKqgYY2CkJVaSquSYb4aJo';
+
+let nome = {};
+let listaMsgsRecebidas = [];
+let msgs = [];
+let resolicitarNome = 0;
+let nomeDigitado = "";
+let listaDeParticipantes = [];
+let iniciaAtualizacaoPagina = 0;
+
+function abrirListaParticipantes() {
     const participantes = document.querySelector('.lista');
     participantes.classList.remove('hide-list');
+
+    const promessa = axios.get('https://mock-api.driven.com.br/api/vm/uol/participants');
+    promessa.then(resposta => {listaDeParticipantes = resposta.data; console.log(listaDeParticipantes)});
 }
+
+function fecharListaParticipantes() {
+    const participantes = document.querySelector('.lista');
+    participantes.classList.add('hide-list');
+}
+
+function manterConexao(){
+    const promessa = axios.post('https://mock-api.driven.com.br/api/vm/uol/status', nome);
+    promessa.then(resposta => console.log('Conexão mantida'));
+}
+
+function solicitarNome(){
+    if(resolicitarNome < 1) {
+        nomeDigitado = prompt("Digite seu nome:\n");
+    } else {
+        nomeDigitado = prompt("O nome digitado já está em uso\nDigite outro nome:\n");
+    }    
+
+    nome = {
+        name: nomeDigitado
+    }
+
+    const promessa = axios.post('https://mock-api.driven.com.br/api/vm/uol/participants', nome);
+    promessa.then(respostaEnvioNome);
+    promessa.catch(erroEnvioNome);       
+}
+
+function respostaEnvioNome(resposta){ 
+    console.log(resposta) 
+    if(resposta.status === 200) {
+        setInterval(manterConexao, 5000);
+    }
+}
+
+function erroEnvioNome(erro){
+    if(erro.response.status === 400){
+        resolicitarNome++;
+        solicitarNome();
+    }
+}
+
+function enviarMsg() {
+    let campoEnviarMsg = document.querySelector('.mensagem-envio');
+
+    let novaMsg = {
+        from: nome.name,
+        to: "Todos",
+        text: campoEnviarMsg.value,
+        type: "message"
+    };
+
+    campoEnviarMsg.value = "";
+
+    const promessa = axios.post('https://mock-api.driven.com.br/api/vm/uol/messages', novaMsg);
+    promessa.then(resposta => console.log(resposta));
+    promessa.catch(resposta => console.log(resposta));
+
+    renderizarMsgs();
+}
+
+function receberMsgs(resposta){
+    listaMsgsRecebidas = resposta.data;
+
+    const ulMsgs = document.querySelector('.mensagens');
+    ulMsgs.innerHTML = '';
+
+    for( let i = 0; i < listaMsgsRecebidas.length; i++){
+        let msg = listaMsgsRecebidas[i];
+        
+        if((msg.text === "entra na sala...") || (msg.text === "sai da sala...")) {
+            ulMsgs.innerHTML += `
+            <li data-test="message">
+                <span class="time">(${msg.time})</span>&#160<span class="nome">${msg.from}</span>&#160${msg.text}
+            </li>
+        `;
+        } else {
+            ulMsgs.innerHTML += `
+            <li data-test="message">
+                <span class="time">(${msg.time})</span>&#160<span class="nome">${msg.from}&#160</span>para&#160<span class="nome">${msg.to}:&#160</span>${msg.text}
+            </li>
+        `;
+        }
+    }
+}
+
+function renderizarMsgs(){
+    const ulMsgs = document.querySelector('.mensagens');    
+
+    const promessa = axios.get('https://mock-api.driven.com.br/api/vm/uol/messages');
+    promessa.then(receberMsgs);
+
+    if(!iniciaAtualizacaoPagina){
+        setInterval(renderizarMsgs, 3000);
+        iniciaAtualizacaoPagina++;
+    }
+}
+
+//solicitarNome();
+renderizarMsgs();
